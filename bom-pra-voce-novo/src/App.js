f039245fe100325fea88df13d6794696273c9215
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2025-06-11 21:13:14 -0300

    Create react app and install tailwindcss

---
+++ bom-pra-voce-novo/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex items-center justify-center">
+      <h1 className="text-4xl text-white font-bold">Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
