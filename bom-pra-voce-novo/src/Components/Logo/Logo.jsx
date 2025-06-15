3abcf042721c3a736fdb1f497efd07d804e7ebb7
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2025-06-15 14:44:09 -0300

    `Refactor Logo component and adjust NavBar layout`

--- bom-pra-voce-novo/src/Components/Logo/Logo.jsx
+++ bom-pra-voce-novo/src/Components/Logo/Logo.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import LogoImage from "../../images/Logo.png";
 
 const Logo = () => {
   return (
     <div className="flex items-center space-x-2">
-      <div className="w-8 h-8 rounded-full flex items-center justify-center">
-        <img src="" alt="logo-bom-pra-voce" />
+      <div className="h-10 rounded-full flex items-center justify-center">
+        <img
+          src={LogoImage}
+          alt="logo-bom-pra-voce"
+          className="w-full h-full object-cover"
+        />
       </div>
     </div>
   );
--- bom-pra-voce-novo/src/Components/NavBar/NavBar.jsx
+++ bom-pra-voce-novo/src/Components/NavBar/NavBar.jsx
@@ -6,9 +6,13 @@ const NavBar = () => {
   return (
     <nav className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 w-[90%] max-w-7xl">
       <div className="backdrop-blur-sm bg-white/30 border border-white/55 rounded-full px-6 py-3 shadow-2xl">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center">
           <Logo />
-          <RoutesNavBar />
+          <div className="flex-1 flex justify-center">
+            <RoutesNavBar />
+          </div>
+          {/* elemento vazio imitando o tamanho da logo para centralizar as rotas */}
+          <div></div>
         </div>
       </div>
     </nav>
