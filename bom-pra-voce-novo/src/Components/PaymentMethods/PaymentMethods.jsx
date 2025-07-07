10a8906d4d327b04e0a748896acedde3b48d9940
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2025-07-07 19:50:16 -0300

    ok

--- bom-pra-voce-novo/src/Components/PaymentMethods/PaymentMethods.jsx
+++ bom-pra-voce-novo/src/Components/PaymentMethods/PaymentMethods.jsx
@@ -2,19 +2,25 @@ import React from "react";
 
 export default function PaymentsMethods() {
   return (
-    <div
-      className="bg-yellow-400 flex items-center justify-center w-full h-[400px]"
-      style={{
-        clipPath: "ellipse(100% 60% at 50% 100%)",
-      }}>
-      <div className="p-4">
-        <div className="flex items-center flex-col mt-10 mb-4 text-center">
-          <h1 className="text-4xl font-extrabold text-white">
-            Escolha a Melhor Forma de Pagar!!
-          </h1>
+    <>
+      <div
+        className="bg-yellow-400 flex items-center justify-center w-full h-[100px]"
+        style={{
+          clipPath: "ellipse(100% 60% at 50% 100%)",
+        }}>
+        <div className="">
+          <div className="flex items-center flex-col mt-10 text-center">
+            <h1 className="text-4xl font-extrabold text-white">
+              Escolha a Melhor Forma de Pagar!!
+            </h1>
+          </div>
         </div>
-        <div className="flex gap-4"></div>
       </div>
-    </div>
+      <div className="bg-yellow-400 flex items-center justify-center w-full h-[400px]">
+        <div className="p-4">
+          <div className="flex gap-4">sss</div>
+        </div>
+      </div>
+    </>
   );
 }
