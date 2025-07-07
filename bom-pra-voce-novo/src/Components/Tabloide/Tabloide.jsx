111a98216f9ef8ef3baba7bc06e9b311890b9377
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2025-07-07 17:32:09 -0300

    `Added import of Tabloide component and commented out Cards component in Home.jsx`

---
+++ bom-pra-voce-novo/src/Components/Tabloide/Tabloide.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Tabloide() {
+  return (
+    <div className="bg-yellow-300 flex items-center w-full h-full">
+      <div className=""></div>
+    </div>
+  );
+}
