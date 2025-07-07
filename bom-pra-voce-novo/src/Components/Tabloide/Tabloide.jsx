d7351a793be577237cbb61d37c3cff3634e5b09b
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2025-07-07 18:12:05 -0300

    `Added header and two banner sections to Tabloide component`

--- bom-pra-voce-novo/src/Components/Tabloide/Tabloide.jsx
+++ bom-pra-voce-novo/src/Components/Tabloide/Tabloide.jsx
@@ -4,7 +4,17 @@ export default function Tabloide() {
   return (
     <div className="bg-white flex items-center justify-center w-full h-full">
       <div className="p-4">
+        <div className="flex items-center flex-col mb-4 text-center">
+          <h1 className="text-4xl font-extrabold text-blue-700">
+            Corre que dá tempo!
+          </h1>
+          <p className="text-lg text-gray-800 font-bold mt-2 ">
+            As melhores ofertas da semana te esperam aqui.
+          </p>
+        </div>
+        {/* cada div dessa tera uma imagem de "banner" para abrir o tabloide vulgo panfleto da semana ou do mes  */}
         <div className="flex gap-4">
+          {/* primeiro panfleto */}
           <div className="relative h-32 lg:h-40 overflow-hidden rounded-lg bg-yellow-400">
             <img
               src="#"
@@ -12,6 +22,7 @@ export default function Tabloide() {
               className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-[1.05]"
             />
           </div>
+          {/* segundo panfleto */}
           <div className="relative h-32 lg:h-40 overflow-hidden rounded-lg bg-yellow-400">
             <img
               src="#"
