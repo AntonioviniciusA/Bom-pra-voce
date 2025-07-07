02d92306d3902ed24e4c2b697f180743d3a1405b
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2025-07-07 17:41:24 -0300

    `Refactor Tabloide component: update styles and layout`

--- bom-pra-voce-novo/src/Components/Tabloide/Tabloide.jsx
+++ bom-pra-voce-novo/src/Components/Tabloide/Tabloide.jsx
@@ -2,8 +2,25 @@ import React from "react";
 
 export default function Tabloide() {
   return (
-    <div className="bg-yellow-300 flex items-center w-full h-full">
-      <div className=""></div>
+    <div className="bg-white flex items-center justify-center w-full h-full">
+      <div className="p-4">
+        <div className="flex gap-4">
+          <div className="relative h-32 lg:h-40 overflow-hidden rounded-lg bg-yellow-400">
+            <img
+              src="#"
+              alt=""
+              className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-[1.05]"
+            />
+          </div>
+          <div className="relative h-32 lg:h-40 overflow-hidden rounded-lg bg-yellow-400">
+            <img
+              src="#"
+              alt=""
+              className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-[1.05]"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- bom-pra-voce-novo/src/pages/Home.jsx
+++ bom-pra-voce-novo/src/pages/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import NavBar from "../Components/NavBar/NavBar";
 import BannerHome from "../Components/BannerHome/BannerHome";
 import Cards from "../Components/Cards/Cards";
-import tabloide from "../Components/Tabloide/Tabloide";
+import Tabloide from "../Components/Tabloide/Tabloide";
 
 export default function Home() {
   return (
     <main className="min-h-screen  flex flex-col items-center ">
       <NavBar />
       <BannerHome />
+      <Tabloide />
       {/* <Cards /> */}
     </main>
   );
