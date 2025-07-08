ae9437a8d09b72433ced952c80f6f6976a23f4d9
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2025-07-08 20:52:03 -0300

    upadate image for background Payments section

--- bom-pra-voce-novo/src/Components/PaymentMethods/PaymentMethods.jsx
+++ bom-pra-voce-novo/src/Components/PaymentMethods/PaymentMethods.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CardPayments from "../CardPayments/CardPayments";
+import FOTO from "../../images/fundo.png";
 
 export default function PaymentsMethods() {
   return (
@@ -22,7 +23,7 @@ export default function PaymentsMethods() {
         </div>
       </div>
 
-      <div className="relative bg-yellow-400 flex flex-col items-center justify-center w-full p-14 overflow-hidden">
+      {/* <div className="relative bg-yellow-400 flex flex-col items-center justify-center w-full p-14 overflow-hidden">
         <div className="absolute bottom-0 left-0 w-full pointer-events-none z-0">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path
@@ -30,7 +31,14 @@ export default function PaymentsMethods() {
               fillOpacity="1"
               d="M0,32L48,32C96,32,192,32,288,64C384,96,480,160,576,181.3C672,203,768,181,864,160C960,139,1056,117,1152,122.7C1248,128,1344,160,1392,176L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
           </svg>
-        </div>
+        </div> */}
+
+      <div className="relative bg-yellow-400 flex items-center justify-center w-full p-14 overflow-hidden">
+        <img
+          src={FOTO}
+          alt="Ilustração de fundo"
+          className="absolute bottom-0 left-0 w-full  opacity-45 z-0"
+        />
 
         <div className="pt-4 pb-12 z-10">
           <div className="flex gap-4">
